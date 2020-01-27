8948ccc1b6af59aa8d7a595b76a9a6e20d768a84
Deivid Soares <deividstandard@gmail.com>
2020-01-27 17:01:16 -0300

    Criação das viewsets

--- gve/api/views.py
+++ gve/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import Cdc
+from .models import Carro
+from .models import Registro
+
+from rest_framework import viewsets
+from rest_framework import generics
+from .serializers import SerializadorCdc
+from .serializers import SerializadorCarro
+from .serializers import SerializadorRegistro
 
 # Create your views here.
+
+
+class Carro(viewsets.ModelViewSet):
+
+    queryset = Carro.objects.all()
+    serializer_class = SerializadorCarro
+
+class Registro(viewsets.ModelViewSet):
+
+    queryset = Registro.objects.all()
+    serializer_class = SerializadorRegistro
+
+class Cdc(viewsets.ModelViewSet):
+
+    queryset = Cdc.objects.all()
+    serializer_class = SerializadorCdc
--- gve/gve/urls.py
+++ gve/gve/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from api import views
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls import url
+from rest_framework import routers
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+router = routers.SimpleRouter()
+router.register(r'carros', views.Carro)
+urlpatterns = router.urls
